612f584dc545f7163900025d1b9e6407749b29ac
Jonata Serpa <jonataserpa@gmail.com>
2023-11-30 14:22:25 -0300

    fix: remove upload

--- src/infra/http/controllers/upload.controller.ts
+++ src/infra/http/controllers/upload.controller.ts
@@ -38,30 +38,6 @@ export class UploadController {
     private readonly removeUploadUseCase: RemoveUploadUseCase,
   ) {}
 
-  @Post('/product')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      dest: './uploads',
-      limits: { fieldNameSize: 300, fileSize: 50485760 }, // limit total size to 5MB
-      storage: multer.diskStorage({
-        destination: './uploads',
-        filename(request, file, callback) {
-          const uniqueSuffix =
-            Date.now() + '_' + Math.round(Math.random() * 1e9);
-          const filename = `${uniqueSuffix}-${file.originalname}`;
-
-          return callback(null, filename);
-        },
-      }),
-    }),
-  )
-  async uploadProduct(@UploadedFile() file) {
-    const upload = {
-      file,
-    };
-    return this.createUploadProductUseCase.execute(upload);
-  }
-
   @Post('/user')
   @UseInterceptors(
     FileInterceptor('file', {
@@ -86,114 +62,9 @@ export class UploadController {
     return this.createUploadUserUseCase.execute(upload);
   }
 
-  @Post('/announcement/:id')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      dest: './uploads',
-      limits: { fieldNameSize: 300, fileSize: 50485760 }, // limit total size to 5MB
-      storage: multer.diskStorage({
-        destination: './uploads',
-        filename(request, file, callback) {
-          const uniqueSuffix =
-            Date.now() + '_' + Math.round(Math.random() * 1e9);
-          const filename = `${uniqueSuffix}-${file.originalname}`;
-
-          return callback(null, filename);
-        },
-      }),
-    }),
-  )
-  async upload(
-    @UploadedFile() file,
-    @Param('id') id: number,
-  ): Promise<UploadResponse> {
-    const upload = {
-      id,
-      file,
-    };
-    return await this.createUploadUseCase.execute(upload);
-  }
-
   @Post('multiples')
   @UseInterceptors(FilesInterceptor('files'))
   uploadMultiple(@UploadedFiles() files: Array<Express.Multer.File>) {
     console.log(files);
-  }
-
-  @Delete('/announcement/:id')
-  remove(@Param('id') id: number) {
-    return this.removeUploadUseCase.execute({ id });
-  }
-
-  @Get('/announcement/:filename')
-  downloadAnnouncement(
-    @Param('filename') filename: string,
-    @Res({ passthrough: true }) res: Response,
-  ): StreamableFile {
-    try {
-      const extension = filename.split('.')[1] === 'jpeg';
-      let file = 'image/jpeg';
-      if (extension) {
-        file = 'image/png';
-      }
-      const readableStream = createReadStream(
-        join(process.cwd(), `uploads/${filename}`),
-      );
-      res.set({
-        'Content-Type': file,
-      });
-      const streamdata = new StreamableFile(readableStream);
-      return streamdata;
-    } catch (error) {
-      return error.message;
-    }
-  }
-
-  @Get('/user/:filename')
-  downloadUser(
-    @Param('filename') filename: string,
-    @Res({ passthrough: true }) res: Response,
-  ): StreamableFile {
-    try {
-      const extension = filename.split('.')[1] === 'jpeg';
-      let file = 'image/jpeg';
-      if (extension) {
-        file = 'image/png';
-      }
-      const readableStream = createReadStream(
-        join(process.cwd(), `uploads/${filename}`),
-      );
-      res.set({
-        'Content-Type': file,
-      });
-      const streamdata = new StreamableFile(readableStream);
-      return streamdata;
-    } catch (error) {
-      return error.message;
-    }
-  }
-
-  @Get('/product/:filename')
-  downloadProduct(
-    @Param('filename') filename: string,
-    @Res({ passthrough: true }) res: Response,
-  ): StreamableFile {
-    try {
-      const extension = filename.split('.')[1] === 'jpeg';
-      let file = 'image/jpeg';
-      if (extension) {
-        file = 'image/png';
-      }
-      const readableStream = createReadStream(
-        join(process.cwd(), `uploads/${filename}`),
-      );
-      res.set({
-        'Content-Type': file,
-      });
-      const streamdata = new StreamableFile(readableStream);
-      return streamdata;
-    } catch (error) {
-      return error.message;
-    }
   }
 }
